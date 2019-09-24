43a6a27db45cb80f9ea5fa1a2a83d6bc50247d40
Lee <dasarup621@gmail.com>
2019-09-24 21:52:05 +0530

    commiting firs java class varibelandjavatype

---
+++ BasicCorejava-src/src/varibaleanddatatypes/Firstjavaclass.java
@@ -0,0 +1,29 @@
+package varibaleanddatatypes;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+		
+		//widely used date types
+		int a=10;//for integer values
+		char b='A';//for single characters
+		boolean c=true;//for boolean values
+		double d=6.89;//for decimal values
+		String name="anand";//for string values**string is not a data
+				
+				
+				//other date types;
+				//for numbers
+				byte x=10;
+		        short y=45;
+		        long z=6796896;
+		        
+		        //for decimal values
+		        float f=67.8f;
+
+		        System.out.println(a);
+		        
+	}
+	
+
+}
