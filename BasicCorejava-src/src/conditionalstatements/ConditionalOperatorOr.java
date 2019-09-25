853e293fb336c33b87b7c69a91e069decc75109a
Lee <dasarup621@gmail.com>
2019-09-25 22:03:31 +0530

    Today  some condition

---
+++ BasicCorejava-src/src/conditionalstatements/ConditionalOperatorOr.java
@@ -0,0 +1,25 @@
+package conditionalstatements;
+
+public class ConditionalOperatorOr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int pcm = 60;
+		int jre = 30;
+		int cet = 60;
+
+		if (pcm >= 60) {
+			if (jre >= 30 || cet >= 60) {
+				System.out.println("you are eligible");
+
+			} else
+				System.out.println("you are not eligble");
+
+		} else {
+
+			System.out.println("you are not eligble");
+		}
+
+	}
+}
---
+++ BasicCorejava-src/src/conditionalstatements/Conditionalstatements1.java
@@ -0,0 +1,28 @@
+package conditionalstatements;
+
+public class Conditionalstatements1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=10;
+		
+		System.out.println(a>b);
+		
+		System.out.println(b>a);
+		
+		if(a>b)
+		{
+			
+			System.out.println("a is greater than b");
+			
+			
+		}else {
+			
+			System.out.println("b is greater then a");
+		}
+
+	}
+
+}
